9cddc3ddb8bf03666862260aff42481eb6bea884
RishabTomar <Rishabtomar@gmail.com>
2020-10-22 16:12:05 +0530

    added hello.javva file in git

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*class Hello{
+	public static void main(String ar[])
+	{
+		System.out.println("\"Hello world...\"");
+	}
+}*/
+
+class Hello{
+	public static void main(String ar[])
+	{
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			System.out.println(i + " : Hello world...");
+		}
+	}
+}
